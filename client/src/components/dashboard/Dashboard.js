31bf7b7bd1e94dcb2f1c9e232e8adbc42e3bdac5
Kevin <knphillips01@gmail.com>
2019-10-09 12:41:34 -0500

    created user login, logout and navbar links, protected routesm reducer and get cur rent profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment, useEffect } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = props => {
-  return <div>
-      Dashboard
-    </div>;
-}
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading }
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
 
-Dashboard.propTypes = {}
+  return <div>Dashboard</div>
+};
 
-export default Dashboard
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile })(Dashboard);
