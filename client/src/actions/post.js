3b9ebf4a0097128636e266769a51e9fe8b9f5c1e
Kevin <knphillips01@gmail.com>
2019-10-11 14:33:20 -0500

    added ability to add posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,6 +5,10 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
+  ADD_POST,
+  //GET_POST,
+  //ADD_COMMENT,
+  //REMOVE_COMMENT
  
 } from './types';
 
@@ -70,6 +74,31 @@ export const deletePost = id => async dispatch => {
     });
 
     dispatch(setAlert('Post Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Add post
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  try {
+    const res = await axios.post('/api/posts', formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Post Created', 'success'));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
